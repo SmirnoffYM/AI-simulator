5cb979d92afbdc3889b2ffd4f052b29deeee16b5
Smirnoff Yuriy <y.m.smirnoff@gmail.com>
2012-03-24 01:23:51 +0200

    implemented local map displaying

--- robotwindow.cpp
+++ robotwindow.cpp
@@ -2,6 +2,7 @@
 #include "ui_robotwindow.h"
 #include "hubmodule.h"
 #include <QCloseEvent>
+#include <math.h>
 
 RobotWindow::RobotWindow(QWidget *parent) :
     QWidget(parent),
@@ -13,6 +14,8 @@ RobotWindow::RobotWindow(QWidget *parent) :
     localMapScene = new QGraphicsScene();
     setWindowFlags(Qt::CustomizeWindowHint | Qt::WindowTitleHint | Qt::WindowStaysOnTopHint );
     ui->robotGraphicsView->setScene(localMapScene);
+    ui->robotGraphicsView->setHorizontalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
+    ui->robotGraphicsView->setVerticalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
     isIntersectionTypeDisplayed = false;
 }
 
@@ -23,7 +26,110 @@ RobotWindow::~RobotWindow()
 
 void RobotWindow::onRefreshMap()
 {
-    //TODO: refresh local map
+    // Draw map
+    Robot *robot = new Robot(HubModule::modellingSystem->getRobot(robotId-1));
+    localMapScene->clear();
+
+    // get top left and bottom right corners of the local map
+    int x1 = robot->getCoords().first - ui->robotGraphicsView->geometry().size().width() / 2;
+    int y1 = robot->getCoords().second - ui->robotGraphicsView->geometry().size().height() / 2;
+    int x2 = robot->getCoords().first + ui->robotGraphicsView->geometry().size().width() / 2;
+    int y2 = robot->getCoords().second + ui->robotGraphicsView->geometry().size().height() / 2;
+
+    //FIXME: frakin map is not centered correctly
+    for (int i = x1 / REAL_PIXEL_SIZE; i <= x2 / REAL_PIXEL_SIZE; i++) {
+        for (int j = y1 / REAL_PIXEL_SIZE; j <= y2 / REAL_PIXEL_SIZE; j++) {
+
+            std::pair<int, int> mapSize = HubModule::modellingSystem->getWorld()->getSize();
+            int heightOfMap = 255;
+            if (i >= 0 && j >= 0 && i < mapSize.first && j < mapSize.second)
+                heightOfMap = HubModule::modellingSystem->getWorld()->getHeight(i, j);
+            QColor pixelColor = QColor(heightOfMap, heightOfMap, heightOfMap);
+
+            // We must "calibrate" local map to robot's position,
+            // so just truncate some map pieces from left and top sides.
+            // As the result robot will be on the map's center
+
+            int width = REAL_PIXEL_SIZE;
+            if ((i * REAL_PIXEL_SIZE < x1 && i >= 0) || (i * REAL_PIXEL_SIZE > x1 && i < 0))
+                width -= x1 - i * REAL_PIXEL_SIZE;
+
+            int height = REAL_PIXEL_SIZE;
+            if ((j * REAL_PIXEL_SIZE < y1 && j >= 0) || (j * REAL_PIXEL_SIZE > y1 && j < 0))
+                height -= y1 - j * REAL_PIXEL_SIZE;
+
+            localMapScene->addRect((i+1) * REAL_PIXEL_SIZE - width,
+                                   (j+1) * REAL_PIXEL_SIZE - height,
+                                   width, height, QPen(pixelColor), QBrush(pixelColor));
+        }
+    }
+
+    delete robot;
+
+    // Draw robots
+    // we must draw only robots inside our localmap rectangle
+    for (int i = 0; i < ROBOTS; i++) {
+        Robot *robot = new Robot(HubModule::modellingSystem->getRobot(i));
+
+        if (robot->getCoords().first >= (int)robot->getSize() / 2
+                && robot->getCoords().second >= (int)robot->getSize() / 2
+                && robot->getCoords().first >= x1 && robot->getCoords().first <= x2
+                && robot->getCoords().second >= y1 && robot->getCoords().second <= y2) {
+
+            QColor outlineColor(255 - robot->getColor().red(),
+                                255 - robot->getColor().green(),
+                                255 - robot->getColor().blue());
+            localMapScene->addEllipse(robot->getCoords().first - robot->getSize() / 2,
+                                      robot->getCoords().second - robot->getSize() / 2,
+                                      robot->getSize(), robot->getSize(),
+                                      QPen(outlineColor), QBrush(robot->getColor()));
+
+            double new_x = robot->getSize() / 2.0 * sin(robot->getOrientation() * PI / 180);
+            double new_y = robot->getSize() / 2.0 * cos(robot->getOrientation() * PI / 180);
+            localMapScene->addLine(robot->getCoords().first, robot->getCoords().second,
+                                   robot->getCoords().first + new_x,
+                                   robot->getCoords().second - new_y,
+                                   QPen(outlineColor));
+        }
+
+        delete robot;
+    }
+
+    // Draw envObjects
+    // we must draw only envObjects inside our localmap rectangle
+    for (int i = 0; i < ENV_OBJECTS; i++) {
+        EnvObject *envObject = new EnvObject(HubModule::modellingSystem->getEnvObject(i));
+
+        if (envObject->getCoords().first >= (int)envObject->getSize() / 2
+                && envObject->getCoords().second >= (int)envObject->getSize() / 2
+                && envObject->getCoords().first >= x1 && envObject->getCoords().first <= x2
+                && envObject->getCoords().second >= y1 && envObject->getCoords().second <= y2) {
+
+            QColor outlineColor(255 - envObject->getColor().red(),
+                                255 - envObject->getColor().green(),
+                                255 - envObject->getColor().blue());
+            localMapScene->addEllipse(envObject->getCoords().first - envObject->getSize() / 2,
+                                      envObject->getCoords().second - envObject->getSize() / 2,
+                                      envObject->getSize(), envObject->getSize(),
+                                      QPen(outlineColor), QBrush(envObject->getColor()));
+
+            // draw orientation line if object is movable
+            if (envObject->isMovable()) {
+                double new_x = envObject->getSize() / 2.0 *
+                        sin(envObject->getOrientation() * PI / 180);
+                double new_y = envObject->getSize() / 2.0 *
+                        cos(envObject->getOrientation() * PI / 180);
+
+                localMapScene->addLine(envObject->getCoords().first,
+                                       envObject->getCoords().second,
+                                       envObject->getCoords().first + new_x,
+                                       envObject->getCoords().second - new_y,
+                                       QPen(outlineColor));
+            }
+        }
+
+        delete envObject;
+    }
 
     refreshRobotParams();
 }
