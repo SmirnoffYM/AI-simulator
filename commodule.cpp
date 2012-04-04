437ae7ac3fc71ffb18ed748ae09d16dcede5d77d
Alexander Batischev <eual.jp@gmail.com>
2012-04-04 11:08:29 +0300

    Remove JSON parsing code and messages-helpers

--- commodule.cpp
+++ commodule.cpp
@@ -27,45 +27,9 @@ void ComModule::handleMessage()
         socket->readDatagram(datagram.data(), datagram.size(),
                         &sender, &senderPort);
         
-        QJson::Parser parser;
-        QVariantMap message = parser.parse(datagram).toMap();
-
-        // parse JSON and put the message into queue
-        MessageType type = stringToMessageType(message["type"].toString());
-        Message *msg = NULL;
-        switch(type) {
-            case MsgMove:
-                msg = new MessageMove();
-                break;
-
-            case MsgTurn:
-                msg = new MessageTurn();
-                break;
-
-            case MsgChangeSize:
-                msg = new MessageChangeSize();
-                break;
-
-            case MsgChangeColor:
-                msg = new MessageChangeColor();
-                break;
-
-            case MsgWhoIsThere:
-                msg = new MessageWhoIsThere();
-                break;
-            
-            default:
-                // TODO: insert QDebug here?
-                break;
-        }
-        if(msg != NULL) {
-            QJson::QObjectHelper::qvariant2qobject(message, msg);
-            // FIXME: QQueue is not thread-safe, we need a mutex here once com module is moved to
-            // separate thread
-            messageQueue->enqueue(msg);
-        } else {
-            // FIXME: stick some QDebug in here
-        }
+        // FIXME: QQueue is not thread-safe, we need a mutex here once com module is moved to
+        // separate thread
+        // messageQueue->enqueue(msg);
     }
 }
 
