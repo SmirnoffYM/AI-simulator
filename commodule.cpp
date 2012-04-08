dd7a4798ab8a7e22f95e1847cf50af0a4dbb81d4
Alexander Batischev <eual.jp@gmail.com>
2012-04-08 17:37:25 +0300

    Add message handling stub

--- commodule.cpp
+++ commodule.cpp
@@ -28,6 +28,49 @@ void ComModule::handleMessage()
                         &sender, &senderPort);
         
         Message *msg = new Message();
+
+        /* QDataStream would handle endianness for us */
+        QDataStream stream(&datagram, QIODevice::WriteOnly);
+
+        quint8 version;
+        stream >> version;
+        if(version != 1) {
+            // FIXME: stick qDebug in here
+            return;
+        }
+
+        quint32 seq_num;
+        quint16 port;
+        quint8 msg_type;
+        stream >> seq_num >> port >> msg_type;
+        msg->num = seq_num;
+        msg->port = port;
+        msg->type = (MessageType)msg_type;
+
+        switch(msg->type) {
+        case MsgAcknowledge:
+            break;
+        case MsgMove:
+            break;
+        case MsgTurn:
+            break;
+        case MsgChangeSize:
+            break;
+        case MsgChangeColor:
+            break;
+        case MsgWhoIsThere:
+            break;
+        case MsgBump:
+            break;
+        case MsgThereYouSee:
+            break;
+        case MsgParameterReport:
+            break;
+        default:
+            // FIXME: qDebug
+            break;
+        }
+
         // FIXME: QQueue is not thread-safe, we need a mutex here once com module is moved to
         // separate thread
         // messageQueue->enqueue(msg);
