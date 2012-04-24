cfaf69e8e071e308cd1f89f74ec7719a8ea09fa0
Smirnoff Yuriy <y.m.smirnoff@gmail.com>
2012-04-24 18:42:13 +0300

    Implement displaying visibility region for normal robots

--- robotwindow.cpp
+++ robotwindow.cpp
@@ -99,38 +99,39 @@ void RobotWindow::onRefreshMap()
 
 void RobotWindow::paintVisibiltyCircle(Robot *robot)
 {
-    if (robot->getType() == Flying)
-    {
-        bool flag = false;              // is first circle coordinate valid?
-        double prev_x;
-        double prev_y;
-        std::pair<int, int> worldSize = HubModule::modellingSystem->getWorld()->getSize();
+    bool flag = false;              // is first circle coordinate valid?
+    bool correctFlag = false;       // used by "normal" robot for skiping first point
+    double prev_x;
+    double prev_y;
+    std::pair<int, int> worldSize = HubModule::modellingSystem->getWorld()->getSize();
 
-        for (int angle = 0; angle <= 360; angle++) {
-            double new_x = robot->getVisibilityRadius() * sin(angle * PI / 180);
-            double new_y = robot->getVisibilityRadius() * cos(angle * PI / 180);
+    for (int angle = 0; angle <= 361; angle++) {
+        double new_x = robot->getVisibilityRadius() * sin(angle * PI / 180);
+        double new_y = robot->getVisibilityRadius() * cos(angle * PI / 180);
 
-            if (!flag) {
-                if (robot->getCoords().first + new_x >= 0
-                        && robot->getCoords().second - new_y >= 0
-                        && robot->getCoords().first + new_x < worldSize.first * REAL_PIXEL_SIZE
-                        && robot->getCoords().second - new_y < worldSize.second * REAL_PIXEL_SIZE) {
-                    prev_x = new_x;
-                    prev_y = new_y;
-                    flag = true;
-                }
-                continue;
+        if (!flag) {
+            if (robot->getCoords().first + new_x >= 0
+                    && robot->getCoords().second - new_y >= 0
+                    && robot->getCoords().first + new_x < worldSize.first * REAL_PIXEL_SIZE
+                    && robot->getCoords().second - new_y < worldSize.second * REAL_PIXEL_SIZE) {
+                prev_x = new_x;
+                prev_y = new_y;
+                flag = true;
             }
+            continue;
+        }
 
-            if (robot->getCoords().first + new_x < 0)
-                new_x = - robot->getCoords().first;
-            if (robot->getCoords().first + new_x >= worldSize.first * REAL_PIXEL_SIZE)
-                new_x = worldSize.first * REAL_PIXEL_SIZE - robot->getCoords().first;
-            if (robot->getCoords().second - new_y < 0)
-                new_y = robot->getCoords().second;
-            if (robot->getCoords().second - new_y >= worldSize.second * REAL_PIXEL_SIZE)
-                new_y = - worldSize.second * REAL_PIXEL_SIZE + robot->getCoords().second;
+        if (robot->getCoords().first + new_x < 0)
+            new_x = - robot->getCoords().first;
+        if (robot->getCoords().first + new_x >= worldSize.first * REAL_PIXEL_SIZE)
+            new_x = worldSize.first * REAL_PIXEL_SIZE - robot->getCoords().first;
+        if (robot->getCoords().second - new_y < 0)
+            new_y = robot->getCoords().second;
+        if (robot->getCoords().second - new_y >= worldSize.second * REAL_PIXEL_SIZE)
+            new_y = - worldSize.second * REAL_PIXEL_SIZE + robot->getCoords().second;
 
+        if (robot->getType() == Flying)
+        {
             localMapScene->addLine(
                         (robot->getCoords().first + prev_x) / REAL_PIXEL_SIZE,
                         (robot->getCoords().second - prev_y) / REAL_PIXEL_SIZE,
@@ -142,12 +143,82 @@ void RobotWindow::paintVisibiltyCircle(Robot *robot)
 
             prev_x = new_x;
             prev_y = new_y;
+
+        } else {    // if robot type is "Normal"
+
+            bool signX = new_x > 0;
+            bool signY = new_y > 0;
+            bool xFaster = fabs(new_x) > fabs(new_y);
+
+            // if x is increasinf faster than y
+            if (xFaster) {
+
+                //find each point of the line and check is it a mount
+                for (int i = robot->getSize() / 2; i < static_cast<int>(fabs(new_x)) - 1; i++) {
+
+                    double temp_x = pow(-1, static_cast<int>(!signX)) * i;
+                    double temp_y = (temp_x - new_x) * (0 - new_y) / (0 - new_x) + new_y;
+
+                    double temp_x_next = pow(-1, static_cast<int>(!signX)) * (i+1);
+                    double temp_y_next = (temp_x_next - new_x) * (0 - new_y) / (0 - new_x) + new_y;
+
+                    World *world = HubModule::modellingSystem->getWorld();
+                    if (world->getHeight((robot->getCoords().first + temp_x) / REAL_PIXEL_SIZE,
+                                         (robot->getCoords().second - temp_y) / REAL_PIXEL_SIZE) >
+                        world->getHeight((robot->getCoords().first + temp_x_next) / REAL_PIXEL_SIZE,
+                                         (robot->getCoords().second - temp_y_next) / REAL_PIXEL_SIZE)
+                            &&
+                        world->getHeight((robot->getCoords().first + temp_x) / REAL_PIXEL_SIZE,
+                                         (robot->getCoords().second - temp_y) / REAL_PIXEL_SIZE) >
+                        world->getHeight(robot->getCoords().first / REAL_PIXEL_SIZE,
+                                         robot->getCoords().second / REAL_PIXEL_SIZE)) {
+                        new_x = temp_x;
+                        new_y = temp_y;
+                        break;
+                    }
+                }
+            } else {
+
+                for (int j = robot->getSize() / 2; j < static_cast<int>(fabs(new_y)) - 1; j++) {
+
+                    double temp_y = pow(-1, static_cast<int>(!signY)) * j;
+                    double temp_x = (temp_y - new_y) * (0 - new_x) / (0 - new_y) + new_x;
+
+                    double temp_y_next = pow(-1, static_cast<int>(!signY)) * (j+1);
+                    double temp_x_next = (temp_y_next - new_y) * (0 - new_x) / (0 - new_y) + new_x;
+
+                    World *world = HubModule::modellingSystem->getWorld();
+                    if (world->getHeight((robot->getCoords().first + temp_x) / REAL_PIXEL_SIZE,
+                                         (robot->getCoords().second - temp_y) / REAL_PIXEL_SIZE) >
+                        world->getHeight((robot->getCoords().first + temp_x_next) / REAL_PIXEL_SIZE,
+                                         (robot->getCoords().second - temp_y_next) / REAL_PIXEL_SIZE)
+                            &&
+                        world->getHeight((robot->getCoords().first + temp_x) / REAL_PIXEL_SIZE,
+                                         (robot->getCoords().second - temp_y) / REAL_PIXEL_SIZE) >
+                        world->getHeight(robot->getCoords().first / REAL_PIXEL_SIZE,
+                                         robot->getCoords().second / REAL_PIXEL_SIZE)) {
+                        new_x = temp_x;
+                        new_y = temp_y;
+                        break;
+                    }
+                }
+            }
+
+            if (correctFlag)
+                localMapScene->addLine(
+                            (robot->getCoords().first + prev_x) / REAL_PIXEL_SIZE,
+                            (robot->getCoords().second - prev_y) / REAL_PIXEL_SIZE,
+                            (robot->getCoords().first + new_x) / REAL_PIXEL_SIZE,
+                            (robot->getCoords().second - new_y) / REAL_PIXEL_SIZE,
+                            QPen(QColor(robot->getColor().red(),
+                                        robot->getColor().green(),
+                                        robot->getColor().blue())));
+            else
+                correctFlag = true;
+
+            prev_x = new_x;
+            prev_y = new_y;
         }
-
-
-    } else {    // if robot type is "Normal"
-        //TODO: draw visibilty region
-
     }
 }
 
